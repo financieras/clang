a6df4cf751dc15f76e5a9172bda8e7276a97e1a4
financieras <financieras@gmail.com>
2022-05-12 10:02:49 +0200

    hola mundo

---
+++ hola.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hola mundo!!!\n");
+    return 0;
+}
+
+/* Primero compilar creando el binario ejecutable hola
+   Se puede compilar así gcc hola.c -o hola
+   Finalmente ejecutar ponendo ./hola     */
