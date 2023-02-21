1bcdf482f0079eaa505332bc7f0662c3ee1f1b88
apa <apa@linux.localdomain>
2023-02-21 19:44:26 +0100

    variables

---
+++ conversion.c
@@ -0,0 +1,14 @@
+/*
+Conversión de tipos de datos
+https://www.youtube.com/watch?v=RI0zoFnolIY&list=PLpOqH6AE0tNgqknxjMAJ8bX_L1a7lnBaH&index=9&ab_channel=codigofacilito
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 23;
+    printf("%i\n", )
+    return 0;
+}
--- variables.c
+++ variables.c
@@ -12,10 +12,17 @@ int main()
     short e = -1;    // 2 bytes, va de -128...127
     unsigned u = 25; // 2 bytes, 0...65535
     long l = pow(2,32)/2-1; //4 bytes, -2147483648...2147483647
+    float f = 72.534;
+    double d = 12323.877658; // 8 bytes
     printf("%c\n", C);
     printf("%i\n", a);
     printf("%i\n", e);
     printf("%i\n", u);
     printf("%li\n", l);
+    printf("%f\n", f);
+    printf("%.2f\n", f);   // redondeando a 2 decimales
+    printf("%f\n", d);     // usando f de float
+    printf("%lf\n", d);    // usnado lf de long float
+    printf("%.2lf\n", d);  // redondeando a 2 decimales
     return 0;
 }
