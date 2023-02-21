552cd356fc0e40a622a7fc733d77f4ee660e5043
apa <apa@mac.local>
2023-02-21 11:34:02 +0100

    saluda desde Sunset Code

--- hola.c
+++ hola.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Hola mundo!!!\n");
+    printf("¡¡Hola mundo mundial!!\n");
     return 0;
 }
 
 /* Primero compilar creando el binario ejecutable hola
    Se puede compilar así gcc hola.c -o hola
    Finalmente ejecutar ponendo ./hola     */
+
+/* Para compilar y ejecutar en una sola línea:
+gcc filename.c && ./a.out
+Si se usa && el segundo comando solo se ejecuta si el primero ha tenido éxito.
+En lugar de && se puede usar ; y en ese caso se ejecutan siempre los dos comandos.
+ */
---
+++ saluda.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* la función main es la función principal */
+
+int main(void){
+	printf("Hola mundo\n");
+	return 0;
+}
+
+/* Para compilar y ejecutar en una sola línea:
+gcc saluda.c -o saluda && ./saluda
+ */
