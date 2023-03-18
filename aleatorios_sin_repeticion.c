4bdaf8b38278c033250c7e1f3706c5b45cf90af8
apa <apa@linux.localdomain>
2023-03-18 09:24:39 +0100

    genera aleatorios sin repetición

--- aleatorios_sin_repeticion.c
+++ aleatorios_sin_repeticion.c
@@ -1,42 +1,51 @@
 /*
-Generación de un array de enteros aleatorios sin repetición
+    Generación de un array de enteros aleatorios sin repetición
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define TAM 5   // número de elementos del array, definido como una constante
+#define TAM 5   // TAMAÑO: número de elementos del array, definido como una constante
 
 int exist(int r, int arr[]);       // prototipo de función
 
-int main(){
+int main()
+{
     srand(time(NULL));   // semilla
     int arr[TAM];
     int r;
 
-    for(int i=0; i<TAM; i++){
-        do{
+    printf("Aleatorios con posible repetición: ");
+    for(int i=0; i<TAM; i++)
+    {
+        do
+        {
             r = rand() % 10 + 1;
             printf("%i ", r);
         } while (exist(r, arr));
         
         arr[i] = r;
     }
-    printf("\n\nSin repetición:\n");
+    printf("\nSin repetición: ");
 
     for(int i=0; i<TAM; i++){
-         printf("%i\n", arr[i]);
+         printf("%i ", arr[i]);
     }
-
+    printf("\n");
     return 0;
 }
 
 
-int exist(int r, int arr[TAM]){   // si el aleatorio r ya existe en el array retorna 0 y en caso contrario retorna 0
-    for(int i=0; i<TAM; i++){
-        if(r==arr[i]){
-            return 1;
-        }
+/*
+    Función que detecta si el aleatorio está repetido.
+    Si el aleatorio r ya existe en el array retorna 1
+    y en caso contrario retorna 0
+*/
+int exist(int r, int arr[TAM])
+{
+    for(int i=0; i<TAM; i++)
+    {
+        if(r==arr[i]) return 1;
     }
     return 0;
 }
