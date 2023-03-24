c714f5bdad3fa199e5592407999b8925bf554f0a
apa <apa@pop-os.localdomain>
2023-03-24 14:54:55 +0100

    auto save

---
+++ inicial/do_while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define precio 10       // constante
+
+int main(void){
+    float litros;
+    char respuesta;
+
+    do{
+        //
+        printf("¿Cuántos litros desea? ");
+        scanf("%f", &litros);
+        printf("\nEl importe a pagar es: %.2f", litros * precio);
+        printf("\n¿Desea otro cálculo? (s = si / cualquier otra palabra para finalizar): ");
+        scanf("%s", &respuesta);
+    }while(respuesta == 's');   // expresión booleana
+
+    return 0;
+}
